ffb20489d1415e7b288140ab654d1c8560801a99
Mayank Raj <mayankra4@gmail.com>
2025-11-02 17:03:16 +0530

    Initialize React project with Tailwind CSS setup, including essential configurations, styles, and assets.

---
+++ reactcss02/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/footer.css"
+
+const footer = () => {
+  return (
+    <div className='footer'>
+        this is a footer
+    </div>
+  )
+}
+
+export default footer
---
+++ reactcss02/src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='bg-red-600'>header</div>
+  )
+}
+
+export default header
